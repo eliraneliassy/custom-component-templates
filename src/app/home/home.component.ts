96fea3c37955d429839409c8119d9f2af2a2bd1b
Eliran Eliassy <eliran.eliassy@gmail.com>
2020-06-19 10:31:21 +0300

    swiper

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { slides } from './../consumer2/data';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  slides = slides.map(x => x.imageUrl);
 
   constructor() { }
 
--- src/app/swiper/swiper.component.ts
+++ src/app/swiper/swiper.component.ts
@@ -7,7 +7,37 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './swiper.component.html',
   styleUrls: ['./swiper.component.scss']
 })
-export class SwiperComponent {
+export class SwiperComponent implements AfterViewInit {
 
+  @Input() slides: string[];
+
+  slidesPositions: any[];
+  currentItem = 0;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  slide(direction: number) {
+    this.currentItem += direction;
+
+    if (this.currentItem > this.slidesPositions.length - 1) { this.currentItem = 0; }
+    if (this.currentItem < 0) { this.currentItem = this.slidesPositions.length - 1; }
+
+    this.document.querySelector('.slider').scrollTo({
+      left: this.slidesPositions[this.currentItem],
+      behavior: 'smooth'
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.getSlidesPositions();
+  }
+
+  getSlidesPositions() {
+    this.slidesPositions = [];
+    this.document.querySelectorAll('.slide').forEach((div: HTMLElement) => {
+      this.slidesPositions.push(div.offsetLeft);
+    });
+
+  }
 
 }
